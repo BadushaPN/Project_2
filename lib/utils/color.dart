998d136ca0300ad9130937565be233adea134baf
BadushaPN <badushapn4@gmail.com>
2024-01-19 21:54:20 +0530

    updated

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -5,5 +5,5 @@ import 'package:flutter/material.dart';
 const myTheme = Color.fromRGBO(213, 201, 221, 1);
 const iconColor = Colors.black;
 const white = Colors.white;
-const dark = Color.fromARGB(255, 69, 50, 99);
+const dark = Color.fromARGB(255, 123, 45, 248);
 const light = Color.fromRGBO(153, 152, 152, 1);
